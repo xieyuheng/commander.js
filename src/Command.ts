c8fb275e00d5ab0025d9250bdce0284ce5eb4637
Xie Yuheng <xyheme@gmail.com>
2025-06-16 10:48:58 +0800

    up

--- src/Command.ts
+++ src/Command.ts
@@ -1,3 +1,3 @@
-export type Command = {
+export class Command {
   //
 }
--- src/Commander.ts
+++ src/Commander.ts
@@ -1,28 +1,10 @@
 import { type Command } from "./Command.ts"
 
-export type Commander = {
-  name: string
-  commands: Array<Command>
+export class Commander {
+  commands: Array<Command> = []
   defaultCommand?: Command
-  run(): void
+
+  constructor(public name: string) {}
+
+  run(): void {}
 }
-
-export function createCommander(name: string): Commander {
-  const commands: Array<Command> = []
-
-  function run() {
-    return
-  }
-
-  return {
-    name,
-    commands,
-    run,
-  }
-}
-
-// function main() {
-//   const commander = createCommander(name)
-//   commander.use(runCommand)
-//   commander.run()
-// }
